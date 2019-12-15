281105129d4b44f470c699ef3e0250a689cba108
Max Kovalovs <maxkovalovs@gmail.com>
2019-12-15 15:32:15 +0000

    Add an endpoint for scoring posts

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 application = Flask(__name__)
 
@@ -6,6 +6,14 @@ application = Flask(__name__)
 @application.route('/')
 def index():
     return "<h1>Welcome to our server</h1>"
+
+
+@application.route("/score-post", methods=["POST"])
+def score_post():
+    payload = request.args.get("title")
+    # Make the score equal the length of the title for now
+    score = len(payload)
+    return score
 
 
 if __name__ == "__main__":
