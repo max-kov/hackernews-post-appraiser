d8cceb3a1639e99f476ec5d05ca299cc0bc1c3e9
Max Kovalovs <maxkovalovs@gmail.com>
2019-12-19 22:44:34 +0000

    Fetch the requests asynchronously

--- model/tools/fetch_data_to_csv.py
+++ model/tools/fetch_data_to_csv.py
@@ -1,3 +1,4 @@
+import grequests
 import requests
 import json
 import csv
@@ -5,11 +6,14 @@ import csv
 api_url = "https://hacker-news.firebaseio.com/v0/item/{}.json"
 max_item = json.loads(requests.get("https://hacker-news.firebaseio.com/v0/maxitem.json").content)
 csv_columns = ['by', 'id', 'score', 'url', 'title', 'time', 'type']
-entries_to_fetch = 10
+entries_to_fetch = 1000
+
+request_urls = (grequests.get(api_url.format(i)) for i in range(max_item-entries_to_fetch, max_item))
+responses = grequests.map(request_urls)
 
 with open('data.csv', 'w', newline='') as file:
     writer = csv.DictWriter(file, fieldnames=csv_columns, extrasaction='ignore')
     writer.writeheader()
-    for i in range(max_item-entries_to_fetch, max_item):
-        data = json.loads(requests.get(api_url.format(i)).content)
+    for response in responses:
+        data = json.loads(response.content)
         writer.writerow(data)
