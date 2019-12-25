7ec096f545fa884f17fc7eacbfc3bc578568a375
Max Kovalovs <maxkovalovs@gmail.com>
2019-12-25 22:06:59 +0000

    Make ui into a class

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -1,11 +1,15 @@
 'use strict';
  
-const ui = (
-    <div class="container">
-        <h1>Hackernews post appraiser.</h1>
-        <input class="post-title" type="text"/>
-        <div id="input-score"/>
-    </div>
-);
+class App extends React.Component {
+    render() {
+        return (
+            <div class="container">
+                <h1>Hackernews post appraiser.</h1>
+                <input class="post-title" type="text"/>
+                <div id="input-score"/>
+            </div>
+        )
+    }
+}
 
-ReactDOM.render(ui, document.getElementById('root'));
+ReactDOM.render(<App />, document.body);
