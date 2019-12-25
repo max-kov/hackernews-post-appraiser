b60de68a1d3a66cdb563383e3f3484bb9ddec1b7
Max Kovalovs <maxkovalovs@gmail.com>
2019-12-25 23:03:46 +0000

    Fix incorrect param name

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -1,12 +1,12 @@
 'use strict';
+
  
 class App extends React.Component {
     render() {
         return (
-            <div class="container">
+            <div className="container">
                 <h1>Hackernews post appraiser.</h1>
-                <input class="post-title" type="text"/>
-                <div id="input-score"/>
+                <input className="post-title" type="text"/>
             </div>
         )
     }
