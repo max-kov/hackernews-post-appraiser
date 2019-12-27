6cb786fd1d24f89bfa9cde96639047f36ea23cf6
Max Kovalovs <maxkovalovs@gmail.com>
2019-12-27 17:53:24 +0000

    Add TitleText area and Score classes to app.js

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -1,15 +1,31 @@
 'use strict';
 
+import Score from "./score.js"
+import TitleTextArea from "./title-text-area.js"
  
 class App extends React.Component {
-    render() {
-        return (
-            <div className="container">
-                <h1>Hackernews post appraiser.</h1>
-                <input className="post-title" type="text"/>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: ""
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      inputValue: event.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div className="container" onChange={this.handleChange}>
+        <h1>Hackernews post appraiser.</h1>
+        <TitleTextArea/>
+        <Score titleText={this.state.inputValue}/>
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
