72f4b86c1bd29d34f95ace54aa14502a0b7928a4
Max Kovalovs <maxkovalovs@gmail.com>
2019-12-29 17:09:43 +0000

    Add TODO

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -17,6 +17,7 @@ class App extends React.Component {
 
   // arrow function is important here, so that we dont redifine 'this'
   handleChange = event => {
+    // TODO: put a rate limit on the requests
     const self = this;
 
     const newTitle = event.target.value;
