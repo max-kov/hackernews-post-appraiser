058db259fb05b2b9a1aa52b545a1e555496ccce8
Max Kovalovs <maxkovalovs@gmail.com>
2020-01-28 22:54:24 +0000

    Create Tfidf vectorized data

---
+++ model/tools/parse_corpus.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import string
+
+import nltk
+nltk.download('stopwords')
+nltk.download('udhr')
+
+from nltk.corpus import stopwords 
+from nltk.tokenize import TweetTokenizer
+from nltk.stem import SnowballStemmer 
+
+stop_words = stopwords.words("english")
+df = pd.read_csv("data.csv")
+
+translator = str.maketrans("", "", string.punctuation) 
+tokenizer = TweetTokenizer(preserve_case=False)
+stemmer = SnowballStemmer("english", ignore_stopwords=True)
+
+df = df.dropna(subset=["title"])
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+vectorizer = TfidfVectorizer(stop_words=stop_words, tokenizer=tokenizer.tokenize, preprocessor=stemmer.stem)
+X = vectorizer.fit_transform(df.title)
