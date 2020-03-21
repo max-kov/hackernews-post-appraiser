3f6398f8b1839474349aaca53f136fd6cf0d8f7c
Max Kovalovs <maxkovalovs@gmail.com>
2020-03-21 21:26:12 +0000

    Create a new file for every 100 api requests

--- model/tools/fetch_data_to_csv.py
+++ model/tools/fetch_data_to_csv.py
@@ -2,20 +2,34 @@ import grequests
 import requests
 import json
 import csv
+import os
+
+
+os.makedirs("data", exist_ok=True)
 
 api_url = "https://hacker-news.firebaseio.com/v0/item/{}.json"
 max_item = json.loads(requests.get("https://hacker-news.firebaseio.com/v0/maxitem.json").content)
+cur_item = (max_item // 100) * 100
 csv_columns = ['by', 'id', 'score', 'url', 'title', 'time', 'type']
-entries_to_fetch = 1000
+entries_to_fetch = 100
 
-request_urls = (grequests.get(api_url.format(i), timeout=1) for i in range(max_item-entries_to_fetch, max_item))
-responses = grequests.map(request_urls)
 
-with open('data.csv', 'w', newline='') as file:
-    writer = csv.DictWriter(file, fieldnames=csv_columns, extrasaction='ignore')
-    writer.writeheader()
-    for response in responses:
-        if (response is not None and response.status_code == requests.codes.ok):
-            data = json.loads(response.content)
-            if (data is not None and data["type"]=="story"):
-                writer.writerow(data)
+
+while 1:
+    f_name = "data/data_{}.csv".format(cur_item)
+
+    if not os.path.exists(f_name):
+        request_urls = (grequests.get(api_url.format(i), timeout=1) for i in range(max_item-entries_to_fetch, max_item))
+        responses = grequests.map(request_urls)
+
+        with open(f_name, 'w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=csv_columns, extrasaction='ignore')
+            writer.writeheader()
+            for response in responses:
+                if (response is not None and response.status_code == requests.codes.ok):
+                    data = json.loads(response.content)
+                    if (data is not None and data["type"]=="story"):
+                        writer.writerow(data)
+
+    cur_item -= entries_to_fetch
+    print("{} downloaded".format(cur_item))
