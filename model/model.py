e50e02f752b791e0c8366c8a644ca9e41779b0f6
Max Kovalovs <maxkovalovs@gmail.com>
2020-03-21 22:10:29 +0000

    Fix relative paths to model files

--- model/model.py
+++ model/model.py
@@ -1,8 +1,10 @@
 import joblib
+import os, pathlib
 
-model = joblib.load("tools/model.sav")
-vectorizer = joblib.load("tools/vectorizer.sav")
-scaler = joblib.load("tools/scaler.sav")
+model_folder = pathlib.Path(__file__).parent / "tools"
+model = joblib.load(model_folder/"model.sav")
+vectorizer = joblib.load(model_folder/"vectorizer.sav")
+scaler = joblib.load(model_folder/"scaler.sav")
 
 def predict(title):
     return scaler.inverse_transform(model.predict(vectorizer.transform([title])))[0]
