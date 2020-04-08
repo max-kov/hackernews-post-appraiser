fee77a5f8ec37989117a8bdcd945818854f5f494
Max Kovalovs <maxkovalovs@gmail.com>
2020-04-08 23:35:22 +0100

    Remove os module import

--- model/model.py
+++ model/model.py
@@ -1,11 +1,11 @@
 import joblib
-import os, pathlib
+import pathlib
 
 model_folder = pathlib.Path(__file__).parent / "tools"
-model = joblib.load(model_folder/"model.sav")
-vectorizer = joblib.load(model_folder/"vectorizer.sav")
-scaler = joblib.load(model_folder/"scaler.sav")
+model = joblib.load(model_folder / "model.sav")
+vectorizer = joblib.load(model_folder / "vectorizer.sav")
+scaler = joblib.load(model_folder / "scaler.sav")
+
 
 def predict(title):
     return scaler.inverse_transform(model.predict(vectorizer.transform([title])))[0]
-
