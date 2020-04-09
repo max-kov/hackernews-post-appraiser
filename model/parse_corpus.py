c09719407ff2de27d762e3263ae7b5b51fd3f59a
Max Kovalovs <maxkovalovs@gmail.com>
2020-04-09 18:24:10 +0100

    Move model generating file from tools folder

--- model/parse_corpus.py
+++ model/parse_corpus.py
@@ -3,10 +3,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 
-import nltk_tools
-import model_to_file
+from tools import nltk_tools, model_to_file
 
-df = pd.read_csv("data.csv")
+df = pd.read_csv("tools/data.csv")
 df = df.dropna(subset=["title"])
 
 vectorizer = TfidfVectorizer(stop_words=nltk_tools.get_stopwords(),
@@ -19,6 +18,6 @@ y = scaler.fit_transform(df[["score"]])
 
 model = LinearRegression().fit(X, y.ravel())
 
-model_to_file.save_model(model, "model")
-model_to_file.save_model(scaler, "scaler")
-model_to_file.save_model(vectorizer, "vectorizer")
+model_to_file.save_model(model, "model_files/model")
+model_to_file.save_model(scaler, "model_files/scaler")
+model_to_file.save_model(vectorizer, "model_files/vectorizer")
