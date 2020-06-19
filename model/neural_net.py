5640ed48004163b3b34c020a9174326f45c1a46e
Max Kovalovs <maxkovalovs@gmail.com>
2020-06-19 23:08:48 +0100

    Factor out vectorizer into its own file

--- model/neural_net.py
+++ model/neural_net.py
@@ -1,93 +1,23 @@
 import pandas as pd
-from tools import nltk_tools, model_to_file
-import itertools
-import string
+from tools import model_to_file
 import torch
 import torch.nn as nn
 import torch.optim as optim
 
-df = pd.read_csv("tools/data.csv")
+from word_level_torch_vectorizer import process_titles
+
+from model.torch_models.rnn import RNNModel
+
+torch.manual_seed(0)
+
+df = pd.read_csv("tools/data.csv")[:500]
 df = df.dropna(subset=["title"])
 
 vector_size = 10000
 
-
-def process_titles(data):
-    tokenizer = nltk_tools.get_tokenizer().tokenize
-    token_arrs = [tokenizer(title) for title in data.values]
-    index = pd.MultiIndex.from_tuples(
-        [(data.index[i], ii) for i, tokens in enumerate(token_arrs) for ii, _ in enumerate(tokens)],
-        names=["sentance", "word"]
-    )
-    tokens = pd.Series(itertools.chain.from_iterable(token_arrs), index=index)
-
-    # Remove stopwords
-    stopwords = nltk_tools.get_stopwords()
-    tokens = tokens[tokens.map(lambda x: x not in stopwords)]
-    # Remove non-ascii
-    tokens = tokens.map(lambda word: ''.join(filter(lambda char: char in string.ascii_letters, word)))
-    # Remove empty
-    tokens = tokens[tokens != ""]
-    # Stem
-    stemmer = nltk_tools.get_stemmer().stem
-    tokens = tokens.map(lambda word: stemmer(word))
-    # only take first top n words
-    top_words = tokens.value_counts().index[:vector_size]
-    word_table = {word: index for index, word in enumerate(top_words)}
-    tokens = tokens[tokens.map(lambda x: x in word_table)]
-    tokens = tokens.map(word_table).groupby(level=0).apply(lambda x: torch.tensor(x.values))
-    indeces = tokens.index
-    tokens = tokens.reset_index(drop=True)
-
-    return tokens, indeces, word_table
-
-
-X, indeces, word_table = process_titles(df.title)
+X, indeces, word_table = process_titles(df.title, vector_size)
 y = df.score[indeces].reset_index(drop=True)
-
-torch.manual_seed(1)
-
-
-class RNNModel(nn.Module):
-    def __init__(self, input_size):
-        super(RNNModel, self).__init__()
-
-        self.input_size = input_size
-        self.hidden_size = 1000
-        self.embedding_size = 1000
-
-        self.embedding = nn.Embedding(self.input_size, self.embedding_size)
-        self.hidden = nn.RNN(self.embedding_size, self.hidden_size, batch_first=True)
-        self.hidden_to_out = nn.Linear(self.hidden_size, 1)
-
-    def forward(self, input_sentence, lengths):
-        embeddings = self.embedding(input_sentence)
-        packed = nn.utils.rnn.pack_padded_sequence(embeddings, lengths, batch_first=True, enforce_sorted=False)
-        _, hidden = self.hidden(packed)
-        out = self.hidden_to_out(hidden[0]).double()
-
-        return out
-
-
-class LSTMModel(nn.Module):
-    def __init__(self, input_size):
-        super(LSTMModel, self).__init__()
-
-        self.input_size = input_size
-        self.hidden_size = 1000
-        self.embedding_size = 1000
-
-        self.embedding = nn.Embedding(self.input_size, self.embedding_size)
-        self.hidden_layer = nn.LSTM(self.embedding_size, self.hidden_size, batch_first=True)
-        self.hidden_to_out = nn.Linear(self.hidden_size, 1)
-
-    def forward(self, input_sentence):
-        embeddings = self.embedding(input_sentence)
-        _, (hidden, _) = self.hidden_layer(embeddings.unsqueeze(0))
-        out = self.hidden_to_out(hidden[0][0])
-
-        return out
-
+data = pd.concat([X, y], 1)
 
 data_point_count = df.shape[0]
 model = RNNModel(vector_size)
@@ -99,26 +29,27 @@ print("Starting training - word vector length = {}, data points = {}".format(vec
 
 
 def pad_f(batch):
-    return torch.nn.utils.rnn.pad_sequence(batch, batch_first=True), [len(x) for x in batch]
+    X = [x[0] for x in batch]
+    y = torch.Tensor([x[1] for x in batch]).double().unsqueeze(1)
+    lengths = [len(x) for x in X]
+    return torch.nn.utils.rnn.pad_sequence(X, batch_first=True), lengths, y
 
 
-batch_size = 10
-sentances = torch.utils.data.DataLoader(X, batch_size=batch_size, collate_fn=pad_f)
+batch_size = 128
+sentances = torch.utils.data.DataLoader(data.values, batch_size=batch_size, collate_fn=pad_f, shuffle=True)
 
-for i, (sample, lengths) in enumerate(sentances):
+for i, (sample, lengths, ys) in enumerate(sentances):
     optimizer.zero_grad()
 
     out = model(sample, lengths)
-    values = y[i * batch_size: (i + 1) * batch_size].values
-    loss = criterion(out, torch.tensor(values).unsqueeze(1))
+    loss = criterion(out, ys)
 
     loss.backward()
     optimizer.step()
 
-    if i % 20 == 1:
-        print("{} out of {}, Sample \"{}\", score = {}, predicted score {},  loss = {}"
-              .format(i, data_point_count // batch_size, df.title[indeces[i * batch_size]], y[i * batch_size], out[0][0],
-                      loss))
+    print("{} out of {}, Sample \"{}\", score = {}, predicted score {},  loss = {}"
+          .format(i, data_point_count // batch_size, df.title[indeces[i * batch_size]], y[i * batch_size], out[0][0],
+                  loss))
 
-model_to_file.save_model(model, "model_files/rnn_model")
+torch.save(model, "model_files/rnn_model.sav")
 model_to_file.save_model(word_table, "model_files/rnn_word_table")
---
+++ model/word_level_torch_vectorizer.py
@@ -0,0 +1,40 @@
+import itertools
+import string
+
+import pandas as pd
+import torch
+
+from tools import nltk_tools
+
+reserved_tokens = 1
+
+
+def process_titles(data, vector_size, word_table=None):
+    tokenizer = nltk_tools.get_tokenizer().tokenize
+    token_arrs = [tokenizer(title) for title in data.values]
+    index = pd.MultiIndex.from_tuples(
+        [(data.index[i], ii) for i, tokens in enumerate(token_arrs) for ii, _ in enumerate(tokens)],
+        names=["sentance", "word"]
+    )
+    tokens = pd.Series(itertools.chain.from_iterable(token_arrs), index=index)
+
+    # Remove stopwords
+    stopwords = nltk_tools.get_stopwords()
+    tokens = tokens[tokens.map(lambda x: x not in stopwords)]
+    # Remove non-ascii
+    tokens = tokens.map(lambda word: ''.join(filter(lambda char: char in string.ascii_letters, word)))
+    # Remove empty
+    tokens = tokens[tokens != ""]
+    # Stem
+    stemmer = nltk_tools.get_stemmer().stem
+    tokens = tokens.map(lambda word: stemmer(word))
+    # only take first top n words
+    top_words = tokens.value_counts().index[:(vector_size - reserved_tokens)]
+    if word_table is None:
+        word_table = {word: reserved_tokens + index for index, word in enumerate(top_words)}
+    tokens = tokens.map(lambda x: word_table.get(x, 0))
+    tokens = tokens.groupby(level=0).apply(lambda x: torch.tensor(x.values))
+    indeces = tokens.index
+    tokens = tokens.reset_index(drop=True)
+
+    return tokens, indeces, word_table
