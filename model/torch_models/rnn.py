414f42776246d3f29164ff019f24166e9504275d
Max Kovalovs <maxkovalovs@gmail.com>
2020-06-19 23:09:08 +0100

    Factor our model class into its own file

---
+++ model/torch_models/rnn.py
@@ -0,0 +1,22 @@
+from torch import nn as nn
+
+
+class RNNModel(nn.Module):
+    def __init__(self, input_size):
+        super(RNNModel, self).__init__()
+
+        self.input_size = input_size
+        self.hidden_size = 1000
+        self.embedding_size = 1000
+
+        self.embedding = nn.Embedding(self.input_size, self.embedding_size)
+        self.hidden = nn.GRU(self.embedding_size, self.hidden_size, batch_first=True, bidirectional=True)
+        self.hidden_to_out = nn.Linear(self.hidden_size, 1)
+
+    def forward(self, input_sentence, lengths):
+        embeddings = self.embedding(input_sentence)
+        packed = nn.utils.rnn.pack_padded_sequence(embeddings, lengths, batch_first=True, enforce_sorted=False)
+        _, hidden = self.hidden(packed)
+        out = self.hidden_to_out(hidden[0]).double()
+
+        return out
