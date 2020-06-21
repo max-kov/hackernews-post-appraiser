19dfdfd18fd0ccd19b0c12e8348ea4e74e411fe6
Max Kovalovs <maxkovalovs@gmail.com>
2020-06-21 22:41:13 +0100

    Move torch requirement from requirements to appstart

--- app_start.sh
+++ app_start.sh
@@ -1,3 +1,4 @@
+pip install torch==1.5.1+cpu -f https://download.pytorch.org/whl/torch_stable.html
 npx babel frontend/src --out-dir frontend --presets react-app/prod
 export PYTHONPATH="${PYTHONPATH}:$(pwd)/model"
 bin/start-nginx gunicorn -c config/gunicorn.conf.py wsgi
