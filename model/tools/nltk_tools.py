baa8e39296ce4a30635b01fc0d331242e850db6e
Max Kovalovs <maxkovalovs@gmail.com>
2020-06-21 12:09:57 +0100

    Extract redownload nltk data function

--- model/tools/nltk_tools.py
+++ model/tools/nltk_tools.py
@@ -1,20 +1,22 @@
-def get_stopwords():
-    import nltk
+import nltk
+
+
+def install_deps():
     nltk.download('stopwords')
+    nltk.download('udhr')
+    nltk.download('udhr')
+
+
+def get_stopwords():
     from nltk.corpus import stopwords
     return stopwords.words("english")
 
 
 def get_tokenizer():
-    import nltk
-    nltk.download('udhr')
     from nltk.tokenize import TweetTokenizer
-
     return TweetTokenizer(preserve_case=False)
 
 
 def get_stemmer():
-    import nltk
-    nltk.download('udhr')
     from nltk.stem import SnowballStemmer
     return SnowballStemmer("english", ignore_stopwords=True)
