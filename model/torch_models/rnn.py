74e4163fbb17c1d58f537e4af6dcdaef9e3b944c
Max Kovalovs <maxkovalovs@gmail.com>
2020-06-21 12:10:39 +0100

    Remove bidirectional GRU layer, add 1 Linear layer

--- model/torch_models/rnn.py
+++ model/torch_models/rnn.py
@@ -8,15 +8,18 @@ class RNNModel(nn.Module):
         self.input_size = input_size
         self.hidden_size = 1000
         self.embedding_size = 1000
+        self.l2_size = 100
 
         self.embedding = nn.Embedding(self.input_size, self.embedding_size)
-        self.hidden = nn.GRU(self.embedding_size, self.hidden_size, batch_first=True, bidirectional=True)
-        self.hidden_to_out = nn.Linear(self.hidden_size, 1)
+        self.hidden = nn.GRU(self.embedding_size, self.hidden_size, batch_first=True)
+        self.hidden_to_l2 = nn.Linear(self.hidden_size, self.l2_size)
+        self.hidden_to_out = nn.Linear(self.l2_size, 1)
 
     def forward(self, input_sentence, lengths):
         embeddings = self.embedding(input_sentence)
         packed = nn.utils.rnn.pack_padded_sequence(embeddings, lengths, batch_first=True, enforce_sorted=False)
         _, hidden = self.hidden(packed)
-        out = self.hidden_to_out(hidden[0]).double()
+        l2 = self.hidden_to_l2(hidden[0])
+        out = self.hidden_to_out(l2).double()
 
         return out
