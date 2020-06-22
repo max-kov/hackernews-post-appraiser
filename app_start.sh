8849b714202394b5283d18d3c7c8b01a69cf01fb
Max Kovalovs <maxkovalovs@gmail.com>
2020-06-22 22:10:59 +0100

    Remove torch install from app_start

--- app_start.sh
+++ app_start.sh
@@ -1,4 +1,3 @@
-pip install torch==1.5.1+cpu -f https://download.pytorch.org/whl/torch_stable.html
 npx babel frontend/src --out-dir frontend --presets react-app/prod
 export PYTHONPATH="${PYTHONPATH}:$(pwd)/model"
 bin/start-nginx gunicorn -c config/gunicorn.conf.py wsgi
