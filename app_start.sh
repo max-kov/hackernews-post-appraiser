ddee79100646f7ad1064533cf0fed574663981fe
Max Kovalovs <maxkovalovs@gmail.com>
2020-08-10 19:27:24 +0100

    Use gunicorn instead of flasks builtin server

--- app_start.sh
+++ app_start.sh
@@ -1,3 +1,3 @@
 npx babel frontend/src --out-dir frontend --presets react-app/prod
 export PYTHONPATH="${PYTHONPATH}:$(pwd)/model"
-bin/start-nginx gunicorn -c config/gunicorn.conf.py wsgi
+gunicorn app
